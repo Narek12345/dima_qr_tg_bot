9d5d6fcf52af7cad6a30f884bc9821a02aacabb5
Narek <narekbayanduryan16@gmail.com>
2024-01-28 12:59:02 +0300

    Writed base config.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import os
-from dotenv import load_dotenv
+from aiogram import executor
+from handlers import client
+from config import dp
 
-# Загрузка переменных окружения из файла .env.
-load_dotenv()
+# Регистрируем все хэндлеры.
+client.register_client_handlers(dp)
 
-# Читаем переменные окружения.
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-WATEWMARK_SIZE = os.getenv('WATEWMARK_SIZE')
-PATH_TO_WATERMARK = os.getenv('PATH_TO_WATERMARK')
-CHANNEL_LINK = os.getenv('CHANNEL_LINK')
-CHANNEL_ID = os.getenv('CHANNEL_ID')
+
+if __name__ == '__main__':
+	executor.start_polling(dp)
